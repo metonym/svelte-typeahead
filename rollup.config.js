f7289c588e9fa73293dd63d9db931b28c213dc76
Eric Liu <ericyl.us@gmail.com>
2024-11-20 09:11:31 -0800

    build!: drop bundled ESM/UMD support

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import svelte from "rollup-plugin-svelte";
 import svelteReadme from "svelte-readme";
-import pkg from "./package.json";
 
-export default () => {
-  if (!process.env.BUNDLE) {
-    return svelteReadme({
-      plugins: [commonjs()],
-      style: `
-        .code-fence li + li { margin: 0; }
-        .code-fence { min-height: 16rem; }
-        .code-fence pre { margin-bottom: 0; margin-top: 12px; }
-      `,
-    });
-  }
-
-  return ["es", "umd"].map((format) => {
-    const UMD = format === "umd";
-
-    return {
-      input: pkg.svelte,
-      output: {
-        format,
-        file: UMD ? pkg.main : pkg.module,
-        name: UMD ? pkg.name : undefined,
-        exports: "named",
-      },
-      plugins: [svelte({ emitCss: false }), resolve(), commonjs()],
-    };
-  });
-};
+export default svelteReadme({
+  plugins: [commonjs()],
+  style: `
+    .code-fence li + li { margin: 0; }
+    .code-fence { min-height: 16rem; }
+    .code-fence pre { margin-bottom: 0; margin-top: 12px; }
+  `,
+});
