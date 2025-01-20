09fdda6eb3f62a4e0ea0e010761f70bd8b2ea898
Eric Liu <ericyl.us@gmail.com>
2025-01-20 10:29:50 -0800

    chore(deps-dev): add `culls` to prune package metadata

--- scripts/prune-package.ts
+++
@@ -1,9 +0,0 @@
-const pkgJson = await Bun.file("./package.json").json();
-
-// Remove unrelated metadata from package.json for publishing.
-delete pkgJson.scripts;
-delete pkgJson.devDependencies;
-
-await Bun.write("./package.json", JSON.stringify(pkgJson, null, 2) + "\n");
-
-export {};
